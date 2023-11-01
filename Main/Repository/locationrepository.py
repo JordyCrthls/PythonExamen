09a31778de02d1a8956b04f2f9f24b7fe203e95a
unknown <jordy.corthals@student.vives.be>
2023-11-01 23:26:04 +0100

    search if location is allready pressent

--- Main/Repository/locationrepository.py
+++ Main/Repository/locationrepository.py
@@ -8,5 +8,20 @@ class Locationrepository:
 	def __init__(self, connection):
 		self.__connection = connection
 
-	def get_new_id(self):
+	def get_new_id():
 		query = "count(*) from locations"
+
+	def location_exists(self, location_name):
+		query= "select 1 from location where id like ?"
+
+		try:
+			cursor = self.__connection.get_connection().cursor()
+			cursor.execute(query, (location_name.strip(),))
+			result = cursor.fetchall()
+		except Exception as e:
+			print('error', e)
+
+		return len(result) >= 1
+
+	def add_location(self):
+		print('location added')
--- Setup/setup.py
+++ Setup/setup.py
@@ -1,6 +1,7 @@
 #Setup.setup.py
 
 import pandas as pd
+import numpy as np
 import sys, csv, os
 from Setup.setuprepository import Repo
 from Main.Repository.mainrepository import DatabaseConnection
@@ -29,7 +30,10 @@ class Setup:
 	def fill_db(self):
 		if(self.__src_file == None):
 			raise Exception('Er is geen csv bestandsnaam opgegeven, geef de naam op via het \'set_src\' commando')
-		cleaned_data = self.cleaning_data()
+		metadata, cleaned_up = self.cleaning_data()
+		exists = self.__locationrepo.location_exists(self.find_valueofmeta(metadata, 'location'))
+		if not exists:
+			self.__locationrepo.add_location()
 
 	def cleaning_data(self):
 		path = os.getcwd()
@@ -42,4 +46,17 @@ class Setup:
 		except Exception as e:
 			print('faild to open file due to ', e)
 			exit(-1)
+
+		filtered_meta = []
+		for sublist in metadata:
+			filtere_sub = []
+			for item in sublist:
+				if item not in filtere_sub:
+					filtere_sub.append(item)
+			filtered_meta.append(filtere_sub)
+		metadata = [f for f in filtered_meta if len(f) == 2]
+
 		return (metadata, cleaned_up)
+
+	def find_valueofmeta(self, data, field):
+		return np.array([meta for meta in data if field in meta]).flatten()[1]
